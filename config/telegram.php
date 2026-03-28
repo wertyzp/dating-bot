22aa5ea0f6b1336fa9e69d903010d2a04cf8d163
Dmytro Hniezdilov <wertyzp@gmail.com>
2026-03-28 11:54:25 +0200

    fix

--- config/telegram.php
+++ config/telegram.php
@@ -9,13 +9,7 @@ return [
     ],
     'commands' => [
         \Werty\Http\Clients\TelegramBot\Types\BotCommandScopeAllPrivateChats::class => [
-            ['command' => 'reset', 'description' => 'Warning! deletes all bot stored data'],
-            ['command' => 'map', 'description' => 'Show message forwarding map'],
-            ['command' => 'schedule', 'description' => 'Configure schedule'],
-        ],
-        \Werty\Http\Clients\TelegramBot\Types\BotCommandScopeAllChatAdministrators::class => [
-            ['command' => 'source', 'description' => 'Set group as source'],
-            ['command' => 'dest', 'description' => 'Select source for this group'],
+            ['command' => 'help', 'description' => 'Show some help'],
         ],
     ]
 ];
